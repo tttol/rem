54540624d715fc7bdff5327383c587b6536b77ed
t5-takahashi <nnc-287@nintendo.co.jp>
2025-08-26 17:21:03 +0900

    add function file roughly

---
+++ src-tauri/src/file.rs
@@ -0,0 +1,15 @@
+fn open() {
+    // Open the file
+}
+
+fn close() {
+    // Close the file
+}
+
+fn read() {
+    // Read from the file
+}
+
+fn write() {
+    // Write to the file
+}
---
+++ src-tauri/src/sync.rs
@@ -0,0 +1,7 @@
+fn export() {
+    // Export the file to an external service(such as Amazon S3)
+}
+
+fn import() {
+    // Import the file from an external service(such as Amazon S3)
+}
