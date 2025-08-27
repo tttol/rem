d33057c8d4eca0432088e1e7afcb9578274c5e05
t5-takahashi <nnc-287@nintendo.co.jp>
2025-08-28 05:55:02 +0900

    fix Cargo.tom and file.rs etc:wq

--- src-tauri/src/file.rs
+++ src-tauri/src/file.rs
@@ -1,18 +1,21 @@
+use std::{fs::File, io::{Read, Write}};
+
 // Composition over inheritance!
 // https://medium.com/comsystoreply/28-days-of-rust-part-2-composition-over-inheritance-cab1b106534a
 // https://tyfkda.github.io/blog/2020/09/27/composition-over-inheritance.html
-fn open() {
-    // Open the file
+fn read(path: &str) -> std::io::Result<()> {
+    let mut f = File::open(path)?;
+    let mut data = vec![];
+    f.read_to_end(&mut data)?;
+    Ok(())
+
 }
 
-fn close() {
-    // Close the file
+fn create(data: &str, filename: &str, path: &str) -> std::io::Result<()> {
+    let mut f = File::create_new(format!("{}/{}", path, filename))?;
+    f.write_all(data.as_bytes())?;
+    Ok(())
 }
 
-fn read() {
-    // Read from the file
-}
-
-fn write() {
-    // Write to the file
+fn write(data: &str, filename: &str) {
 }
