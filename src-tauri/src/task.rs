90e8540aca2d57a0fe87e41d124ef2e997b7d629
tttol <tkhstol.929@gmail.com>
2025-09-02 06:00:24 +0900

    update tasks.rs

--- src-tauri/src/task.rs
+++ src-tauri/src/task.rs
@@ -1,23 +1,39 @@
 use std::{fs, path::PathBuf};
 use tauri::{AppHandle, Manager};
-use log::{info, warn};
+use log::{info};
 use crate::file;
 
-pub fn get_all(app_handle: &AppHandle) -> Result<String, Box<dyn std::error::Error>> {
+pub fn get_all(app_handle: &AppHandle) -> Result<Vec<String>, Box<dyn std::error::Error>> {
     let app_data_dir = app_handle.path().app_data_dir().map_err(|e| {
         std::io::Error::new(std::io::ErrorKind::Other, e)
     })?;
     info!("app_data_dir={}", app_data_dir.display());
 
     // create a directory if it does not exist.
-    std::fs::create_dir_all(&app_data_dir.join("todo"))?;
-    std::fs::create_dir_all(&app_data_dir.join("doing"))?;
-    std::fs::create_dir_all(&app_data_dir.join("done"))?;
-    std::fs::create_dir_all(&app_data_dir.join("pending"))?;
-    
+    let todo_path = app_data_dir.join("todo");
+    let doing_path = app_data_dir.join("doing");
+    let done_path = app_data_dir.join("done");
+    let pending_path = app_data_dir.join("pending");
+    std::fs::create_dir_all(&todo_path);
+    std::fs::create_dir_all(&doing_path);
+    std::fs::create_dir_all(&done_path);
+    std::fs::create_dir_all(&pending_path);
+
+    let todos = get_tasks(&todo_path)?;
+    let doings = get_tasks(&doing_path)?;
+    let dones = get_tasks(&done_path)?;
+    let pendings = get_tasks(&pending_path)?;
+
+    let mut all_tasks = Vec::new();
+    all_tasks.extend(todos);
+    all_tasks.extend(doings);
+    all_tasks.extend(dones);
+    all_tasks.extend(pendings);
+
+    Ok(all_tasks)
 }
 
-fn get_tasks(target_path: &PathBuf) -> Result<(), Box<dyn std::error::Error>>{
+fn get_tasks(target_path: &PathBuf) -> Result<Vec<String>, Box<dyn std::error::Error>>{
     let mut tasks = vec![];
     for entry in fs::read_dir(target_path)? {
         let entry = entry?;
@@ -25,27 +41,22 @@ fn get_tasks(target_path: &PathBuf) -> Result<(), Box<dyn std::error::Error>>{
         if path.is_dir() {
             continue;
         }
-        // let file_name_result = entry.file_name().into_string()?;
-        // match file_name_result {
-        //     Ok(file_name) => read_file(&file_name, target_path),
-        //     Err(e) => format!("Error converting filename: {}", e)
-        // }
-
-        // let file_name = entry.file_name().into_string()?;
         
-        let file_name = entry.file_name().into_string()
-                        .map_err(|e| format!("Failed to converting into string. {}", e))?;
+        let file_name = entry
+            .file_name()
+            .into_string()
+            .map_err(|e| format!("Failed to converting into string. {:?}", e))?;
         match file::read_single_file(&file_name, &path) {
             Ok(content) => tasks.push(content),
-            Err(e) => format!("Error reading file: {}", e),
+            Err(e) => eprintln!("Error reading file: {}", e),
         }
     }
-    return tasks;
+    return Ok(tasks);
 }
 
-fn read_file(filename: &str, path: &PathBuf) -> String {
-    match file::read_single_file(filename, path) {
-        Ok(content) => content,
-        Err(e) => format!("Error reading file: {}", e),
-    }
-}
+// fn read_file(filename: &str, path: &PathBuf) -> String {
+//     match file::read_single_file(filename, path) {
+//         Ok(content) => content,
+//         Err(e) => format!("Error reading file: {}", e),
+//     }
+// }
