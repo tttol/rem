7b7e53211471dd6bb122e29df024ff42ec04209b
tttol <tkhstol.929@gmail.com>
2025-09-03 13:36:35 +0900

    add reload button

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,10 +6,11 @@ import Lane from "./Lane";
 import { Task } from "../dto";
 import { Status } from "../enum";
 import Header from "./Header";
+import { IoReload } from "react-icons/io5";
 
 function App() {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [message, setMessage] = useState<string>();
+  const [server_message, setMessage] = useState<string>();
 
   const updateTaskStatus = (taskId: string, newStatus: Status) => {
     setTasks(prevTasks =>
@@ -19,22 +20,31 @@ function App() {
     );
   };
 
-  useEffect(() => {
-    console.log("App started");
+  const readTasks = () => {
     invoke<Task[]>('get_all_task').then((fetchedTasks) => {
       setTasks(fetchedTasks);
-      setMessage(`Loaded ${fetchedTasks.length} tasks`);
     }).catch(err => {
-      console.error("Failed to fetch tasks:", err);
-      setMessage("Failed to load tasks");
+      setMessage(`Failed to fetch tasks. {$err}`);
     });
+  }
+
+  useEffect(() => {
+    reload();
   }, []);
+
+  const reload = () => {
+    setTasks([]);
+    readTasks();
+  }
 
   return (
     <>
       <Header />
-      <p>{message}</p>
+      <p>{server_message}</p>
       <main className="flex justify-center w-full px-2">
+        <div onClick={reload}>
+          <IoReload />
+        </div>
         <div className="flex">
           <Lane tasks={tasks} statusLabel={Status.TODO} updateTaskStatus={updateTaskStatus} />
           <Lane tasks={tasks} statusLabel={Status.DOING} updateTaskStatus={updateTaskStatus} />
